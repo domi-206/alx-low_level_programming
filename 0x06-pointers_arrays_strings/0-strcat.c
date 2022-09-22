0e5c68959d712925eb0e059c6ccbdb97c85ae28d
ABAH <abahdominic19@gmail.com>
2022-09-21 17:05:11 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,24 @@
-#include "main.h"                                                                                                                               
-                                                                                                                                               
-/**                                                                                                                                             
- *_strcat - concatenates the string to by @sre to                                                                                             
- * the end of the string pointed to by @dest                                                                                                    
- *@dest: String that will be appended                                                                                                           
- *@sre: String to be concatenated upon                                                                                                          
- *                                                                                                                                              
- *Return: retuns poiner to @dest                                                                                                                
- */                                                                                                                                                                                                                                                                                          
-char *_strcat(char *dest, char *sre)                                                                                                            
-{                                                                                                                      
+#include "main.h"
 
-	int index = 0, dest_len = 0;	
-                                                                                                                                             
-        while(dest[index++])                                                                                                                    
-                dest_len++;                                                                                                                     
-                                                                                                                                                       for(index = 0; sre[index]; index++)                                                                                                     
-                dest[dest_len++] = src[index];                                                                                                 																		       
-        return (dest);                                                                                                                          
+/**
+ *_strcat - concatenates the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int dest = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
